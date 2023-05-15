78ea3d5f22a5a4075f0c3f3a61443c81d6510947
Daniel-Mingoranse <50594387+Daniel-Mingoranse@users.noreply.github.com>
2023-05-15 13:54:00 -0300

    2 files modified, 1 added

--- src/app/pages/modify/modify.component.ts
+++ src/app/pages/modify/modify.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-modify',
   templateUrl: './modify.component.html',
-  styleUrls: ['./modify.component.css']
+  styleUrls: ['./modify.component.css'],
 })
 export class ModifyComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
   ngOnInit() {
+    
   }
-
+  clear() {
+   let input  = document.querySelectorAll('input');
+   console.log(input)
+   //  for(let i=0; i<= input.lenght; i++){
+  //    input[i].value = 'null'
+  //  }
+  }
 }
