75a6ff2ee265c6c2575c668b934444c184b5c142
Daniel-Mingoranse <50594387+Daniel-Mingoranse@users.noreply.github.com>
2023-05-17 17:02:55 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,21 @@ import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RouterModule } from '@angular/router';
 import { ModifyComponent } from './pages/modify/modify.component';
-
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
 
 
 @NgModule({
-  imports: [CommonModule, BrowserModule, RouterModule.forRoot([
+  imports: [CommonModule, BrowserModule, FormsModule, AngularFireModule.initializeApp({
+    apiKey: "AIzaSyDM0qdnyp7EXP_Ax4XR0-FtAy52P9nSgr8",
+    authDomain: "stockflow-62902.firebaseapp.com",
+    databaseURL: "https://stockflow-62902-default-rtdb.firebaseio.com",
+    projectId: "stockflow-62902",
+    storageBucket: "stockflow-62902.appspot.com",
+    messagingSenderId: "245886100906",
+    appId: "1:245886100906:web:36603f5341cb538b8d37d3",
+    measurementId: "G-WWTZCWTPC4"
+  }) ,RouterModule.forRoot([
     {path: '',component: HomeComponent},
     {path: 'listing',component: ListingComponent},
     {path: 'register',component: RegisterComponent},
